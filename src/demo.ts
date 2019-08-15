3892fbe765f6f86e75c79379ab69587bf131a9ff
谢腾远 <redbuck@126.com>
2019-08-15 17:07:25 +0800

    修复在设置了box-sizing:content-box;的页面下,宽度不正确的问题

--- src/demo.ts
+++ src/demo.ts
@@ -1,58 +1,58 @@
 // import Toast from '../lib/toast'
 // import '../lib/toast.css'
 
-import Toast from './package/main'
+import Toast from './package/main';
 
 const {extendAlias} = Toast;
 const $ = (selector: string): Element => document.querySelector(selector);
 
 $('#btn').addEventListener('click', function () {
-    Toast('hello world')
+  Toast('hello world');
 });
 $('#delay').addEventListener('click', function () {
-    Toast({
-        message: 'hello world',
-        duration: 4000,
-    });
+  Toast({
+    message: 'hello world',
+    duration: 4000,
+  });
 });
 $('#modal').addEventListener('click', function () {
-    Toast({
-        message: 'hello world',
-        mask: true,
-    });
+  Toast({
+    message: 'hello world',
+    mask: true,
+  });
 });
 $('#type').addEventListener('click', function () {
-    Toast({
-        type: 'icon-music',
-        message: 'hello world',
-    });
+  Toast({
+    type: 'icon-music',
+    message: 'hello world',
+  });
 });
 $('#mount').addEventListener('click', function () {
-    Toast({
-        message: 'hello world',
-        mask: true,
-        mountTo: () => document.getElementById('box'),
-    });
+  Toast({
+    message: 'hello world',
+    mask: true,
+    mountTo: () => document.getElementById('box'),
+  });
 });
 
 $('#success').addEventListener('click', function () {
-    Toast.success('hello world')
+  Toast.success('hello world');
 });
 $('#error').addEventListener('click', function () {
-    Toast.error('hello world')
+  Toast.error('hello world');
 });
 $('#loading').addEventListener('click', function () {
-    const t = Toast.loading('hello world');
-    setTimeout(() => {
-        t.clear();
-    }, 1000)
+  const t = Toast.loading('hello world');
+  setTimeout(() => {
+    t.clear();
+  }, 1000);
 });
 
 $('#clearAll').addEventListener('click', function () {
-    Toast.clearAll();
+  Toast.clearAll();
 });
 
 const myToast = extendAlias({type: 'icon-music', duration: 1000, mask: true});
 $('#myToast').addEventListener('click', function () {
-    myToast('asdf')
+  myToast('asdf');
 });
